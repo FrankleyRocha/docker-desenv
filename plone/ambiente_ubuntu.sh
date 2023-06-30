276b901190351653c5aec0776e33e368156e2672
Frankley Francalino da Rocha <contato.frankley@gmail.com>
2023-06-30 10:58:16 -0300

    ok

--- plone/ambiente_ubuntu.sh
+++ plone/ambiente_ubuntu.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install -y \
+        build-essential \
+        zlib1g-dev \
+        libncurses5-dev \
+        libgdbm-dev \
+        libnss3-dev \
+        libssl-dev \
+        libreadline-dev \
+        libffi-dev \
+        wget \
+        curl \
+        git \
+        libbz2-dev \
+        libsqlite3-dev \
+        liblzma-dev
+
+curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+source ~/.bashrc
+
 pyenv install 3.10.12
 pyenv global 3.10.12
 
