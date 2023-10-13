d71e9d36e4386c06693c58e2c29a8edd5869d4e9
Frankley Francalino da Rocha <rocha112358@gmail.com>
2023-10-12 21:19:50 -0300

    ok

--- oracle/init-data.sh
+++ oracle/init-data.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sudo rm -rf admin oradata
-mkdir admin oradata
-sudo chown 54321:54321 -R oradata admin
+#sudo rm -rf admin oradata
+#mkdir admin oradata
+#git checkout sqlnet.ora
+#sudo chown 54321:54321 -R oradata admin sqlnet.ora
+#sudo chmod 777 -R oradata admin sqlnet.ora
+
+docker volume rm oracle-data
+#docker volume rm oracle-dpdump
+docker volume create --name=oracle-data
+#docker volume create --name=oracle-dpdump
